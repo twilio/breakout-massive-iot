9eaf26451efc3e46374fb5b81bb803e4f35b9df2
Randy Beiter <rbeiter@twilio.com>
2019-08-26 14:19:09 -0400

    Support TLS with or without root or client certs

--- src/modem/OwlModemSSLRN4.h
+++ src/modem/OwlModemSSLRN4.h
@@ -113,6 +113,9 @@ class OwlModemSSLRN4 {
  private:
   OwlModemAT* atModem_;
   str ssl_response = {.s = nullptr, .len = 0};
+  bool hasDeviceCert = false;
+  bool hasDevicePkey = false;
+  bool hasServerCA = false;
   bool shouldWriteCertificate(usecmng_certificate_type_e type, str name, str new_value);
   int calculateMD5ForCert(char* output, int max_len, str input);
   bool isPEM(str input);
